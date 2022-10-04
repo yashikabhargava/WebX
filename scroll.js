f2c498a0f8bc1dc2e2df6984b0fd9c189260f0a5
Abhishek Anand <abhirk247@gmail.com>
2022-10-04 22:39:45 +0530

    Added scrollReveal library

---
+++ scroll.js
@@ -0,0 +1,24 @@
+
+ScrollReveal().reveal('.container', {
+  scale: 0.85,
+  delay: 375,
+  duration: 500,
+  reset: false
+});
+
+ScrollReveal().reveal('.section', {
+  scale: 0.85,
+  delay: 375,
+  duration: 500,
+  reset: false
+});
+
+ScrollReveal().reveal('#contact-us',{scale:0.65});
+
+
+
+
+
+
+
+

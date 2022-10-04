91d3f9aec6d3fd4f8a7411b9008b370656e684f1
Aishwarya Upadhyay <aishwaryaupadhyay55@gmail.com>
2022-10-04 17:04:12 +0530

    added splash screen

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ checkbox.addEventListener('change', ()=>{
   document.body.classList.toggle('dark');
 
 })
+
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded',(e)=>{
+  setTimeout(()=>{
+    splash.classList.add('display-none');
+  },2000);
+})

ad59afe0b259104e08e2158cc259eeba9ec67940
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2022-10-04 13:50:23 +0530

    Added Scroll button

--- main.js
+++ main.js
@@ -4,3 +4,24 @@ checkbox.addEventListener('change', ()=>{
   document.body.classList.toggle('dark');
 
 })
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 20) {
+      $('#toTopBtn').fadeIn();
+    } else {
+      $('#toTopBtn').fadeOut();
+    }
+  });
+
+  $('#toTopBtn').click(function() {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+  });
+});
+menu.onclick = () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}

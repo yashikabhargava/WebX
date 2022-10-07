e2cdd10cba4ebb1d7c476067ae07faa9666b938b
Rashmi Singh <90052358+Rashmisingh-18@users.noreply.github.com>
2022-10-07 09:08:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,22 @@ checkbox.addEventListener('change', ()=>{
   document.body.classList.toggle('dark');
 
 })
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".right-nav");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".bar-btn");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}

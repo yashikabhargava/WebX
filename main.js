4789c1a651447c76504d5fffd42b933be3aadeee
Aishwarya Upadhyay <aishwaryaupadhyay55@gmail.com>
2022-10-14 20:56:32 +0530

    Loading screen added

--- main.js
+++ main.js
@@ -5,10 +5,20 @@ checkbox.addEventListener('change', ()=>{
 
 })
 
-const splash = document.querySelector('.splash');
 
-document.addEventListener('DOMContentLoaded',(e)=>{
-  setTimeout(()=>{
-    splash.classList.add('display-none');
-  },2000);
-})
+
+
+function delayer() {
+  setTimeout(downLoad, 3000)
+}
+function downLoad() {
+  if (document.all) {
+      document.all["preloader"].style.display = "none";
+      document.all["layer2"].style.display = "block";
+  } else if (document.getElementById) {
+      node = document.getElementById("preloader").style.display = 'none';
+
+      node = document.getElementById("layer2").style.visibility = 'visible';
+
+  }
+}
